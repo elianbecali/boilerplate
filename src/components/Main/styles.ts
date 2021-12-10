f2e08d94f8c10325498e13b47b8a7c1a5a3afb54
Elian Becali <elianbecaliaguiar@gmail.com>
2021-12-09 21:33:56 -0400

    feat(components): create style example

---
+++ src/components/Main/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.main`
+  background-color: #06092b;
+  color: #fff;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+`;
+
+export const Description = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
+`;
